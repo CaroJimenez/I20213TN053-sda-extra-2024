f21a55aa482771279d39a4cfaa85c4ac47f9f78e
CaroJimenez <20213tn053@utez.edu.mx>
2024-04-23 16:45:35 -0600

    first commit

---
+++ src/test/java/com/example/examenrest/ExamenRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.examenrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
